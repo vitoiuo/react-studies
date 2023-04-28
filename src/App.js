ae96fd49bc3378106433b688cb4392b012c96085
Victor Magalhaes <victor.barreto@buser.com.br>
2023-04-27 22:25:09 -0300

    Fazendo fetch em apis usando React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
 
 const App = () => {
-  const [modal, setModal] = React.useState(false)
+  const [item, setItens] = React.useState({})
+  let [loading, setLoading] = React.useState(false)
 
-  return (<>
-    <ButtonModal setModal={setModal} />
-    <Modal modal={modal} setModal={setModal}/>
-  </>)
+  async function fetchTablets (event) {
+    setLoading(true)
+    let data = (await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText.toLowerCase()}`))
+    data = await data.json()
+
+    setItens(data)
+    setLoading(false)
+  }
+  async function fetchSmartphones (event) {
+    setLoading(true)
+    let data = (await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText.toLowerCase()}`))
+    data = await data.json()
+
+    setItens(data)
+    setLoading(false)
+  }
+  async function fetchNotebooks (event) {
+    setLoading(true)
+    let data = (await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText.toLowerCase()}`))
+    data = await data.json()
+
+    setItens(data)
+    setLoading(false)
+  }
+
+  return <>
+    <h1>Produtos</h1>
+    <button onClick={fetchTablets}>Tablet</button>
+    <button onClick={fetchSmartphones}>Smartphone</button>
+    <button onClick={fetchNotebooks}>Notebook</button>
+
+    <div>
+      {loading && <div>Loading...</div>}
+
+      {item.id && (
+         <>
+          <h2>{`${item.nome} (${item.vendido ? "Disponivel": 'Vendido'})`}</h2>
+          {item.fotos?.map((foto) => {
+            return <img key={foto.titulo} alt={foto.titulo} src={foto.src} />
+          })}
+          <p>{ `R$ ${item.preco} - ${item.descricao}`}</p>
+         </>
+      )}
+      
+    </div>
+  </>
 }
 
 export default App
--- src/ButtonModal.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ButtonModal({setModal}) {
-  return (
-    <button onClick={() => setModal(true)}>Abrir modal</button>
-  )
-}
